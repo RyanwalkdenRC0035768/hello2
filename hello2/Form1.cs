34aec3a9fd7799f7aa29e304a61c9b834531805d
Ryan Walkden <rc0035768@student.riversidecollege.ac.uk>
2018-11-19 15:41:48 +0000

    added button and named it hello

--- hello2/Form1.cs
+++ hello2/Form1.cs
@@ -12,9 +12,21 @@ namespace hello2
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1() 
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            messagebox.  show(hllo).
+        
+        }
     }
 }
+
